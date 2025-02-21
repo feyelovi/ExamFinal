7b44c0b5c33e4fa08c540ab60d6aed00a470fbd0
feyelovi <63791275+feyelovi@users.noreply.github.com>
2025-02-21 13:48:56 -0500

    Initial commit

---
+++ src/main/java/com/examenFinal/App.java
@@ -0,0 +1,13 @@
+package com.examenFinal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
